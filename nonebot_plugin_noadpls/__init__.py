aa66c8950fee1d8f093a1fc4f575ce01f649b9ea
gongfuture <gongfuture@outlook.com>
2025-03-11 19:49:01 +0800

    🦄 refactor(version management): 使用_version.py进行版本管理

--- nonebot_plugin_noadpls/__init__.py
+++ nonebot_plugin_noadpls/__init__.py
@@ -6,8 +6,8 @@ require("nonebot_plugin_alconna")
 
 from . import __main__ as __main__
 from .config import ConfigModel
+from ._version import __version__ as __version__
 
-__version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-noadpls",
     description="插件模板",
