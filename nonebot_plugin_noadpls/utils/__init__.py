8c05b67cbb37d744afc2bba9240ba5f6c25aaba6
gongfuture <gongfuture@outlook.com>
2025-03-16 22:59:00 +0800

    ✨ feat(禁言功能): 从yzyyz1387/nonebot_plugin_admin cv群禁言功能及相关逻辑
    https://github.com/yzyyz1387/nonebot_plugin_admin

--- nonebot_plugin_noadpls/utils/__init__.py
+++ nonebot_plugin_noadpls/utils/__init__.py
@@ -5,3 +5,37 @@ from .constants import  GetStorePath, CacheConstants
 __all__ = ["Log", "log", 
            "GetStorePath","CacheConstants"
            ]
+
+
+# Original by https://github.com/yzyyz1387/nonebot_plugin_admin/blob/main/nonebot_plugin_admin/utils.py
+
+
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, ActionFailed, Bot
+from nonebot.matcher import Matcher
+from typing import Union, Optional
+from ..config import  global_config, config
+
+su = global_config.superusers
+
+async def mute_sb(bot: Bot, gid: int, lst: list, time: Optional[int] = None, scope: Optional[list] = None):
+    """
+    构造禁言
+    :param gid: 群号
+    :param time: 时间（s)
+    :param lst: at列表
+    :param scope: 用于被动检测禁言的时间范围
+    :return:禁言操作
+    """
+    if 'all' in lst:
+        yield bot.set_group_whole_ban(group_id=gid, enable=True)
+    else:
+        if time is None:
+            if scope is None:
+                time = random.randint(config.ban_rand_time_min, plugin_config.ban_rand_time_max)
+            else:
+                time = random.randint(scope[0], scope[1])
+        for qq in lst:
+            if int(qq) in su or str(qq) in su:
+                log.info(f"SUPERUSER无法被禁言, {qq}")
+            else:
+                yield bot.set_group_ban(group_id=gid, user_id=qq, duration=time)
