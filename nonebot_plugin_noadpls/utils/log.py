cf5152b32cb11ab3a4f44cc8bb4a9d88c708e8f4
gongfuture <gongfuture@outlook.com>
2025-03-29 10:43:55 +0800

    🌈 style(log): 排除模块开头的 . ，看起来更美观(?

--- nonebot_plugin_noadpls/utils/log.py
+++ nonebot_plugin_noadpls/utils/log.py
@@ -27,7 +27,7 @@ class Log:
         if module:
             # 获取完整模块路径
             module_path = module.__name__
-            return module_path[(module_path.find(".")):]
+            return module_path[(module_path.find(".")) + 1:]
         return "unknown"
 
     def trace(self, msg: Union[str, Any], *args, **kwargs) -> None:
